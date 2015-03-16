0df763c3a35d1b91431db3aecb5630a61d13ec5f
raybit <raybit.tang@gmail.com>
2015-03-16 22:22:48 +0800

    init an almost empty unity project to test

---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("aba");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   Transform tf= this.gameObject.transform;
+       tf.localPosition += new Vector3(0f, 0f, 0.1f);
+
+	}
+}
